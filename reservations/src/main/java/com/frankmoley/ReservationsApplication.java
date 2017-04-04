ab866c3771545f6ffa84629e092beafedc996b00
varun <varunreddyvr@gmail.com>
2017-04-03 21:23:12 -0500

    d

--- reservations/src/main/java/com/frankmoley/ReservationsApplication.java
+++ reservations/src/main/java/com/frankmoley/ReservationsApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ReservationsApplication {
-	//reservation
+	//reservati
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReservationsApplication.class, args);
